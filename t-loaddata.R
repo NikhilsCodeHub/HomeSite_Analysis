af33d8ab0e2710eadc9c4f668a9bc01d15349a99
Nikhil W <wnikhil@gmail.com>
2016-01-15 09:26:36 -0600

    init

---
+++ t-loaddata.R
@@ -0,0 +1,92 @@
+# Load data
+
+library(randomForest)
+library(rpart)
+library(caret)
+
+
+dtrain <- read.csv("train.csv", header = TRUE, na.strings = c(" ", "", "NA"))
+
+
+dtest <- read.csv("test.csv", header = TRUE, na.strings = c(" ", "", "NA"))
+
+# cleaning data and Imputing NA values
+
+na.cols <- sapply(dtrain[,1:dim(dtrain)[2]],anyNA)
+
+na.cols
+
+
+##----- Basic RF Model
+##---
+
+rf_fit <- randomForest(formula = dtrain$QuoteConversion_Flag ~ ., data = dtrain, ntrees = 500)
+
+pred <- predict(rf_fit, dtest)
+
+
+
+
+ImputeData <- function (df_train)
+{
+## Extract the title of the passengers
+
+ptitle_match<-regexpr("Mr\\.|Mrs\\.|Master\\.|Miss\\.|Ms\\.",text =  df_train$Name)
+
+ptitles <- substr(df_train$Name, ptitle_match, ptitle_match+attr(ptitle_match, "match.length")-2)
+
+table(ptitles)
+##    Master   Miss     Mr    Mrs     Ms 
+## 26     40    182    517    125      1 
+
+df_train <- cbind(df_train, data.frame("titles"=ptitles))
+
+df_train$titles[df_train$titles=="" & df_train$Sex=="male" & df_train$Age>12 & !is.na(df_train$Age)] <- "Mr"
+
+df_train$titles[df_train$titles=="" & df_train$Sex=="male" & df_train$Age<13 & !is.na(df_train$Age)] <- "Master"
+
+# If sex=female and ((age>18 and parch=0 and SibSp=0) or (Age<19) then title=Miss
+df_train$titles[df_train$titles=="" & (df_train$Sex=="female" & df_train$Age>18 & !is.na(df_train$Age) & df_train$Parch==0 & df_train$SibSp==0) | (df_train$Age<19)] <- "Miss"
+
+# If sex=female and age>18 and (parch=1 or SibSp=1) then title=Mrs
+df_train$titles[df_train$titles=="" & df_train$Sex=="female" & df_train$Age>18 & !is.na(df_train$Age) & (df_train$Parch==1 | df_train$SibSp==1)] <- "Mrs"
+
+df_train$titles[df_train$titles=="Ms"] <- "Miss"
+
+df_train$titles[df_train$titles=="" & df_train$Sex=="male"] <- "Mr"
+
+df_train$titles <- factor(df_train$titles)
+
+## In case Fare is NA in test data.
+df_train$Fare[is.na(df_train$Fare)] <- mean(df_train$Fare, na.rm = TRUE)
+
+## df_train<- data.frame(df_train$titles, stringsAsFactors = FALSE)
+
+## Identify family groups 
+grouplist <- regexpr(",",text =  df_train$Name)
+
+df_train<- cbind(df_train, data.frame(groupName = substr(df_train$Name, 0,grouplist-1), stringsAsFactors = FALSE))
+
+# As the majority of distribution is 'S'. We assign that to NA.
+df_train[is.na(df_train$Embarked),"Embarked"] <- "S"
+
+df_train$Pclass <- factor(df_train$Pclass)
+
+with_age_df_train <- df_train[!is.na(df_train$Age), ]
+without_age_df_train <- df_train[is.na(df_train$Age), ]
+
+
+# Fitting glm model to impute missing Age
+
+fit_age <- train(Age~Pclass+Sex+SibSp+Parch+Fare+Embarked+titles, data = with_age_df_train, method="glm") 
+
+pred_age <- predict(fit_age, without_age_df_train)
+
+df_train[is.na(df_train$Age),"Age"] <- round(pred_age)
+## Alternative: df_train[without_age_df_train$PassengerId,"Age"] <- round(pred_age)
+
+df_train$Age<-round(df_train$Age)
+
+return(df_train)
+}
+
---
+++ t-ops.R
@@ -0,0 +1,102 @@
+###
+# OPS Scripts that performs the magic
+###
+
+##----- Load, Clean and Impute Data
+##------
+dtrain <- read.csv("train.csv", header = TRUE, na.strings = c(" ", ""))
+na.cols <- sapply(dtrain[,1:dim(dtrain)[2]],anyNA)
+na.cols
+
+dtrain <- ImputeData(dtrain)
+na.cols <- sapply(dtrain[,1:dim(dtrain)[2]],anyNA)
+na.cols
+
+##------ Create Data Partition for Validation
+##------
+lst_train <- createDataPartition(dtrain$Pclass, p = 0.7, list = FALSE)
+
+itrain <- dtrain[lst_train, ]
+ivalidate <- dtrain[-lst_train,]
+
+
+##------ Formula Creation
+##------
+yterm <- "as.factor(Survived)"
+
+xterm <- list(c("Pclass", "Sex", "Age", "SibSp", "Parch"))
+xterm[[2]] <- c("Pclass", "Sex", "SibSp", "Parch")
+xterm[[3]] <- c("Pclass", "Sex", "SibSp", "Parch", "Fare")
+xterm[[4]] <- c("Pclass", "titles", "SibSp", "Parch", "Fare", "Embarked")
+xterm[[5]] <- c("Pclass", "Sex", "titles", "SibSp", "Parch", "Fare", "Embarked")
+xterm[[6]] <- c("Pclass", "Sex", "SibSp", "Parch", "titles")
+
+fmla <- createFormula(yterm, xterm)
+
+
+##------ Model Building
+##------
+
+trCtrl <- trainControl(method = "cv", number = 10)
+mdls1 <- createModels(fmla, itrain, "rf", ntrees=1000, trControl=trCtrl)
+
+mdls2 <- createModels(fmla, itrain, "rpart")
+
+preds1 <- generatePredictions(mdls1, itrain)
+colnames(preds1) <-  paste("c", seq(1:dim(preds1)[2]), sep = "")
+preds2 <- generatePredictions(mdls2, itrain)
+colnames(preds2) <-  paste("d", seq(1:dim(preds2)[2]), sep = "")
+cmbDF <- cbind("Survived" =itrain$Survived, preds1, preds2)
+
+cmbFit <- train(as.factor(Survived)~., data=cmbDF, method="rf", trControl=trCtrl)
+
+## vimp1 <- sapply(mdls1, varImp, simplify = FALSE)
+## vimp2 <- sapply(mdls2, varImp, simplify = FALSE)
+
+
+##------ Validating the combo model on ivalidate dataset
+##------
+
+predsv1 <- generatePredictions(mdls1, ivalidate)
+predsv2 <- generatePredictions(mdls2, ivalidate)
+
+colnames(predsv1) <-  paste("c", seq(1:dim(predsv1)[2]), sep = "")
+colnames(predsv2) <-  paste("d", seq(1:dim(predsv2)[2]), sep = "")
+
+
+cmbDFv <- cbind(predsv1, predsv2 )
+
+predsV <- predict(cmbFit, cmbDFv)
+
+cnfMx <- confusionMatrix(predsV, ivalidate$Survived)
+
+
+##------ Process Test Data
+##------
+
+dtest <- read.csv("test.csv", header = TRUE)
+
+dtest <- ImputeData(dtest)
+na.cols <- sapply(dtest[,1:dim(dtest)[2]],anyNA)
+na.cols
+
+predsTst1 <- generatePredictions(mdls1, dtest)
+predsTst2 <- generatePredictions(mdls2, dtest)
+
+colnames(predsTst1) <-  paste("c", seq(1:dim(predsTst1)[2]), sep = "")
+colnames(predsTst2) <-  paste("d", seq(1:dim(predsTst2)[2]), sep = "")
+
+
+cmbDFTst <- cbind(predsTst1, predsTst2 )
+
+predsTst <- predict(cmbFit, cmbDFTst)
+
+
+
+my_solution <- data.frame(PassengerId = dtest$PassengerId, data.frame(Survived=predsTst))
+
+# Write your solution away to a csv file with the name my_solution.csv
+write.csv(my_solution, file=paste0("Titanic_Submission_", format(Sys.time(), "%m%d%Y%H%M"),".csv") , row.names=FALSE)
+
+
+
