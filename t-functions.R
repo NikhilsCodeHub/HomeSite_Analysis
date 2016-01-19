851915a171459bc8231924c4534a2f3f8618140a
Nikhil <NikhilsCodeHub@users.noreply.github.com>
2016-01-19 11:55:18 -0600

    added new functions
    functions added for HomeSite project

--- t-functions.R
+++ t-functions.R
@@ -29,3 +29,79 @@ calcConfusionMx <- function(predictons, reference){
   return(cnfMx)
 }
 
+
+process_personal_16_17_18_19 <- function(dfData){
+  dfData$PersonalField16A <- substr(x = dfData$PersonalField16,start = 1,stop = 1)
+  dfData$PersonalField16B <- substr(x = dfData$PersonalField16,start = 2,stop = 2)
+  
+  dfData$PersonalField17A <- substr(x = dfData$PersonalField17,start = 1,stop = 1)
+  dfData$PersonalField17B <- substr(x = dfData$PersonalField17,start = 2,stop = 2)
+  
+  dfData$PersonalField18A <- substr(x = dfData$PersonalField18,start = 1,stop = 1)
+  dfData$PersonalField18B <- substr(x = dfData$PersonalField18,start = 2,stop = 2)
+  
+  dfData$PersonalField19A <- substr(x = dfData$PersonalField19,start = 1,stop = 1)
+  dfData$PersonalField19B <- substr(x = dfData$PersonalField19,start = 2,stop = 2)
+  
+  dfData <- dfData[,-c("PersonalPersonalField16", "PersonalPersonalField17", "PersonalPersonalField18", "PersonalPersonalField19")]
+  
+  return(dfData)
+}
+
+check_factor_levels <- function(col_vector){
+  if (length(unique(col_vector)) < 27){
+    rnames <- rownames(table(col_vector))
+    
+    if (length(grepl("Y|N",rnames))==length(rnames) & length(rnames) == 2){
+    col_vector <- factor(col_vector, levels = c("Y", "N"))
+    print("Data is Y|N levels.")
+    }
+    else if (length(grepl("[A-Z]",rnames))==length(rnames)){
+      col_vector <- factor(col_vector, levels = LETTERS)
+      print("Data is A-Z levels.")
+    }
+    
+  }
+  else {
+    
+    
+  }
+  return(col_vector)
+}
+
+impute_missing_values <- function(col_vector){
+  if (typeof(col_vector) == "integer"){
+    col_vector[is.na(col_vector)] <- -10
+  }
+  if (length(unique(col_vector)) < 27){
+    rnames <- rownames(table(col_vector))
+    if (length(grepl("Y|N",rnames))==length(rnames) & length(rnames) == 2){
+      misng <- length(is.na(col_vector)) 
+      ratioYN <- length(c[c=="Y" & !is.na(c)])/length(c[c=="N" & !is.na(c)])
+      if(ratioYN<1){
+        y_counts <- round(length(col_vector[is.na(col_vector)]) * ratioYN)
+        if(y_counts>0){
+          col_vector[is.na(col_vector)][1:y_counts] <- "Y"
+          col_vector[is.na(col_vector)] <- "N"
+        }
+        else{
+          col_vector[is.na(col_vector)] <- "N"
+        }
+      }
+      else {
+        n_counts <- round(length(col_vector[is.na(col_vector)]) / ratioYN)
+        if(n_counts>0){
+          col_vector[is.na(col_vector)][1:n_counts] <- "N"
+          col_vector[is.na(col_vector)] <- "Y"
+        }
+        else{
+          col_vector[is.na(col_vector)] <- "Y"
+        }
+      }
+        
+    }
+  
+  }  
+  return(col_vector)
+}
+
